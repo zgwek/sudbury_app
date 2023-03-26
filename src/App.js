95321855e199fd19ab924eab188dbe00cb89e100
zgweks <92344996+zgwek@users.noreply.github.com>
2023-03-25 21:48:58 -0400

    add template for jsx files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Content from './Content';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header/>
+      <Content/>
+      <Footer/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World
@@ -17,7 +23,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      
     </div>
   );
 }
